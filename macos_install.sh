72e6b242793655c8889830136d30a129bafb4279
Cedric Hutchings <cedric@passivelogic.com>
2023-12-29 19:38:14 -0700

    err well that's versions, not libs

--- macos_install.sh
+++ macos_install.sh
@@ -6,9 +6,9 @@ cmake -S glfw -B glfw_build
 cd glfw_build && make
 cd ..
 
-chrome_libs=("/Applications/Google Chrome.app/Contents/Frameworks/Google Chrome Framework.framework/Versions/"*)
-cp "${chrome_libs[0]}/Libraries/libEGL.dylib" libEGL.dylib
-cp "${chrome_libs[0]}/Libraries/libGLESv2.dylib" libGLESv2.dylib
+chrome_versions=("/Applications/Google Chrome.app/Contents/Frameworks/Google Chrome Framework.framework/Versions/"*)
+cp "${chrome_versions[0]}/Libraries/libEGL.dylib" libEGL.dylib
+cp "${chrome_versions[0]}/Libraries/libGLESv2.dylib" libGLESv2.dylib
 
 cd ..
 
