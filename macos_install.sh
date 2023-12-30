fd481b888cd47f8d657c07aad8a49a9b6bba7af9
Cedric Hutchings <cedric@passivelogic.com>
2023-12-29 18:58:05 -0700

    make chrome version agnostic

--- macos_install.sh
+++ macos_install.sh
@@ -6,8 +6,9 @@ cmake -S glfw -B glfw_build
 cd glfw_build && make
 cd ..
 
-cp "/Applications/Google Chrome.app/Contents/Frameworks/Google Chrome Framework.framework/Versions/120.0.6099.129/Libraries/libEGL.dylib" libEGL.dylib
-cp "/Applications/Google Chrome.app/Contents/Frameworks/Google Chrome Framework.framework/Versions/120.0.6099.129/Libraries/libGLESv2.dylib" libGLESv2.dylib
+chrome_libs=("/Applications/Google Chrome.app/Contents/Frameworks/Google Chrome Framework.framework/Versions/"*)
+cp "${chrome_libs[0]}/Libraries/libEGL.dylib" libEGL.dylib
+cp "${chrome_libs[0]}/Libraries/libGLESv2.dylib" libGLESv2.dylib
 
 cd ..
 
