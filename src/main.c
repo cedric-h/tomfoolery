e0f1d3b349b1aae76768a4af9224d2a60bcdb0b0
Cedric Hutchings <cedric@passivelogic.com>
2024-01-02 18:58:59 -0700

    fix really stupid bug that was breaking mac builds

--- src/main.c
+++ src/main.c
@@ -85,7 +85,7 @@ int main(void) {
     glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-#ifdef WIN32
+#ifndef WIN32
     glfwWindowHint(GLFW_CONTEXT_CREATION_API, GLFW_EGL_CONTEXT_API);
 #endif
 
--- win_build.bat
+++ win_build.bat
@@ -1,7 +1,7 @@
 @echo off
 
 cd build
-clang -o bloom.exe ^
+clang -o bloom.exe -DWIN32 ^
   ..\src\main.c glfw_build/src/Debug/glfw3.lib ^
    -luser32 -lgdi32 -lmsvcrt -lshell32.lib -lvcruntime.lib ^
    -Iglfw/deps -Iglfw/include ^
