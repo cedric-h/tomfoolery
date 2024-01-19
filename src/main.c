f0907e1cb5ec071a1c9137799651d565b5a63cd8
Cedric Hutchings <cedric@passivelogic.com>
2024-01-18 20:49:44 -0700

    gissum culah up in heeah

--- src/main.c
+++ src/main.c
@@ -15,6 +15,11 @@
 typedef unsigned char bool;
 #endif
 
+typedef uint32_t Color;
+#define COLOR_ACTOR  0xff0000ff
+#define COLOR_WOOD   0xff00ff00
+#define COLOR_PLANK  0xffff0000
+
 /* static double lerp(double v0, double v1, double t) { return v0 + t * (v1 - v0); } */
 /* static double inv_lerp(double min, double max, double p) { return (p - min) / (max - min); } */
 
@@ -28,6 +33,7 @@ void norm2(float *x, float *y) {
     *x /= len;
     *y /= len;
 }
+#define COUNT(ARR) ((sizeof(ARR) / sizeof(ARR[0])))
 
 
 /* PLATFORM { */
@@ -57,16 +63,16 @@ static GLuint glayout_vertex_pos;
 static GLuint glayout_vertex_col;
 typedef struct Vertex {
     struct { float x; float y;          } pos;
-    struct { float r; float g; float b; } col;
+    Color rgba;
 } Vertex;
 
 static const char *vertex_shader_text =
 "#version 100\n"
 "precision mediump float;\n"
 "uniform mat4 u_mvp;\n"
-"attribute vec3 v_col;\n"
+"attribute vec4 v_col;\n"
 "attribute vec2 v_pos;\n"
-"varying vec3 a_col;\n"
+"varying vec4 a_col;\n"
 "void main()\n"
 "{\n"
 "    gl_Position = u_mvp * vec4(v_pos, 0.0, 1.0);\n"
@@ -76,10 +82,10 @@ static const char *vertex_shader_text =
 static const char *fragment_shader_text =
 "#version 100\n"
 "precision mediump float;\n"
-"varying vec3 a_col;\n"
+"varying vec4 a_col;\n"
 "void main()\n"
 "{\n"
-"    gl_FragColor = vec4(a_col, 1.0);\n"
+"    gl_FragColor = a_col;\n"
 "}\n";
 
 typedef struct {
@@ -121,8 +127,8 @@ static void geo_gl_vertex_layout(Geo *geo) {
     glEnableVertexAttribArray(glayout_vertex_col);
     glVertexAttribPointer(glayout_vertex_pos, 2, GL_FLOAT, GL_FALSE,
                           sizeof(Vertex), (void*) offsetof(Vertex, pos));
-    glVertexAttribPointer(glayout_vertex_col, 3, GL_FLOAT, GL_FALSE,
-                          sizeof(Vertex), (void*) offsetof(Vertex, col));
+    glVertexAttribPointer(glayout_vertex_col, 4, GL_UNSIGNED_BYTE, GL_FALSE,
+                          sizeof(Vertex), (void*) offsetof(Vertex, rgba));
 }
 
 static void geo_gl_upload(Geo *geo) {
@@ -140,14 +146,14 @@ static void geo_clear(Geo *geo) {
     geo->cpu.ibuf_i = 0;
 }
 
-static void geo_box(Geo *geo, float x, float y, float w, float h) {
+static void geo_box(Geo *geo, float x, float y, float w, float h, Color clr) {
     uint16_t start_i = geo->cpu.vbuf_i;
 
     Vertex br, bl, tl, tr;
-    br.pos.x = x + w/2, br.pos.y = y - h/2, br.col.r = 1.0f, br.col.g = 0.0f, br.col.b = 0.0f;
-    bl.pos.x = x - w/2, bl.pos.y = y - h/2, bl.col.r = 1.0f, bl.col.g = 0.0f, bl.col.b = 0.0f;
-    tl.pos.x = x - w/2, tl.pos.y = y + h/2, tl.col.r = 1.0f, tl.col.g = 0.0f, tl.col.b = 0.0f;
-    tr.pos.x = x + w/2, tr.pos.y = y + h/2, tr.col.r = 1.0f, tr.col.g = 0.0f, tr.col.b = 0.0f;
+    br.pos.x = x + w/2, br.pos.y = y - h/2, br.rgba = clr;
+    bl.pos.x = x - w/2, bl.pos.y = y - h/2, bl.rgba = clr;
+    tl.pos.x = x - w/2, tl.pos.y = y + h/2, tl.rgba = clr;
+    tr.pos.x = x + w/2, tr.pos.y = y + h/2, tr.rgba = clr;
 
     geo->cpu.vbuf[geo->cpu.vbuf_i++] = br;
     geo->cpu.vbuf[geo->cpu.vbuf_i++] = bl;
@@ -162,6 +168,46 @@ static void geo_box(Geo *geo, float x, float y, float w, float h) {
     geo->cpu.ibuf[geo->cpu.ibuf_i++] = 0 + start_i;
 }
 
+static void geo_circ(Geo *geo, float x, float y, float r, Color clr) {
+    uint16_t idx[] = {
+        2, 6, 8,
+        0, 1, 8,
+        1, 2, 8,
+        2, 3, 4,
+        4, 5, 2,
+        5, 6, 2,
+        6, 7, 8
+    };
+
+    struct { float x; float y; } vtx[] = {
+        {  0.0000f,  1.0000f },
+        { -0.6428f,  0.7660f },
+        { -0.9848f,  0.1736f },
+        { -0.8660f, -0.5000f },
+        { -0.3420f, -0.9397f },
+        {  0.3420f, -0.9397f },
+        {  0.8660f, -0.5000f },
+        {  0.9848f,  0.1736f },
+        {  0.6428f,  0.7660f }
+    };
+
+    uint16_t start_i = geo->cpu.vbuf_i;
+
+    int i;
+
+    for (i = 0; i < COUNT(idx); i++) {
+        geo->cpu.ibuf[geo->cpu.ibuf_i++] = idx[i] + start_i;
+    }
+
+    for (i = 0; i < COUNT(vtx); i++) {
+        Vertex v = {0};
+        v.pos.x = x + vtx[i].x * r;
+        v.pos.y = y + vtx[i].y * r;
+        v.rgba = clr;
+        geo->cpu.vbuf[geo->cpu.vbuf_i++] = v;
+    }
+}
+
 /* } GRAPHIX */
 
 
@@ -170,9 +216,12 @@ static void geo_box(Geo *geo, float x, float y, float w, float h) {
 
 typedef unsigned int Ticks;
 
-#define FRAME_SECS (1.0 / 120.0)
-#define ACTOR_MOVE_TICKS_TO_MAX  ((int)floorf(1.0f / FRAME_SECS))
-#define ACTOR_STOP_TICKS_TO_ZERO ((int)floorf(1.0f / FRAME_SECS))
+#define FRAME_SECS               (1.0 / 120.0)
+#define ACTOR_MOVE_TURN_AGILITY  (0.008f)
+#define ACTOR_MOVE_MAX_SPEED     (FRAME_SECS * 5.0f)
+#define ACTOR_MOVE_ACCEL         (0.07f)
+#define ACTOR_STOP_DECEL         (0.15f)
+#define SIM_CAM_SPEED            (FRAME_SECS * 2.5f)
 
 typedef struct {
     double dt;
@@ -221,16 +270,16 @@ void sim_step(Sim *sim, double dt) {
 
             {
                 float speed = sqrtf(sim->actor.vx*sim->actor.vx + sim->actor.vy*sim->actor.vy);
-                float dir_x = sim->actor.vx + dx*0.008f;
-                float dir_y = sim->actor.vy + dy*0.008f;
+                float dir_x = sim->actor.vx + dx*ACTOR_MOVE_TURN_AGILITY;
+                float dir_y = sim->actor.vy + dy*ACTOR_MOVE_TURN_AGILITY;
                 norm2(&dir_x, &dir_y);
 
                 {
-                    float ideal_speed = moving ? (FRAME_SECS * 5.0f) : 0.0f;
+                    float ideal_speed = moving ? ACTOR_MOVE_MAX_SPEED : 0.0f;
                     /* break speed delta apart into sign and magnitude */
                     float delta_mag = fabsf(speed - ideal_speed);
                     float delta_sign = ((speed - ideal_speed) < 0) ? 1 : -1;
-                    float speed_step = FRAME_SECS * (moving ? 0.07f : 0.15f);
+                    float speed_step = FRAME_SECS * (moving ? ACTOR_MOVE_ACCEL : ACTOR_STOP_DECEL);
 
                     if (delta_mag < speed_step) {
                         speed = ideal_speed;
@@ -264,10 +313,10 @@ void sim_step(Sim *sim, double dt) {
                 /* move camera slower if we're already close */
                 {
                     float t;
-                    t = len * FRAME_SECS * 250.0f;
+                    t = len * SIM_CAM_SPEED;
 
-                    dx *= t * 0.01f;
-                    dy *= t * 0.01f;
+                    dx *= t;
+                    dy *= t;
                 }
 
                 sim->cam.x += dx;
@@ -336,7 +385,7 @@ int main(void) {
     }
 
     /* allocate dynamic, per-frame geometry buffer */
-    geo_alloc(&geo, 1 << 8, 1 << 12);
+    geo_alloc(&geo, 1 << 9, 1 << 12);
     sim_init(&sim);
 
     glfwSetTime(0);
@@ -359,15 +408,15 @@ int main(void) {
         /* generate dynamic frame geometry */
         {
             geo_clear(&geo);
-            geo_box(&geo, sim.actor.x, sim.actor.y, 1.0f, 1.0f);
+            geo_box(&geo, sim.actor.x, sim.actor.y, 1.0f, 1.0f, COLOR_ACTOR);
 
             {
                 int i;
-                for (i = 0; i < 20; i++) {
-                    float r = ((float)i / 20.0f) * 3.14159f * 2.0f;
-                    float x = cosf(r) * i;
-                    float y = sinf(r) * i;
-                    geo_box(&geo, x, y, 0.1f, 0.1f);
+                for (i = 0; i < 25; i++) {
+                    float r = ((float)i / 25.0f) * 3.14159f * 2.0f;
+                    float x = cosf(r) * 5.0f;
+                    float y = sinf(r) * 5.0f;
+                    geo_circ(&geo, x, y, 0.6f, COLOR_WOOD);
                 }
             }
 
