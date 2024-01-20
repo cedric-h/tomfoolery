98c8d530b1a5955d0801e9a125810fdc9b564490
Cedric Hutchings <cedric@passivelogic.com>
2024-01-19 17:45:02 -0700

    lines

--- bs/macos_build.sh
+++ bs/macos_build.sh
@@ -1,5 +1,5 @@
 cd build
-cc -o bloom -ansi -Wall -pedantic -Werror -Wno-overlength-strings \
+cc -o bloom -ansi -Wall -pedantic -Werror -Wno-overlength-strings -Wno-error=unused-function \
    -fsanitize=address,undefined -g \
    ../src/main.c glfw_build/src/libglfw3.a \
    -Iglfw/deps -Iglfw/include \
--- src/main.c
+++ src/main.c
@@ -17,12 +17,13 @@ typedef unsigned char bool;
 
 typedef uint32_t Color;
 #if 1
+/* #define DEEP_GRAY 0x958a6ffful */
 #define COLOR_0 0xf4debefful
 #define COLOR_1 0xbfa082fful
 #define COLOR_2 0x4d675afful
 #define COLOR_3 0x847b45fful
 #define COLOR_4 0xf5aa5ffful
-#define COLOR_5 0xc79773fful
+#define COLOR_5 0xca8a6ffful
 #else
 #define COLOR_0 0xf9f7f1fful
 #define COLOR_1 0xf6dbbcfful
@@ -45,6 +46,22 @@ void norm2(float *x, float *y) {
     *y /= len;
 }
 #define COUNT(ARR) ((sizeof(ARR) / sizeof(ARR[0])))
+
+static void mat4x4_ortho(float m[4][4], float l, float r, float b, float t, float n, float f) {
+    m[0][0] = 2.f/(r-l);
+    m[0][1] = m[0][2] = m[0][3] = 0.f;
+
+    m[1][1] = 2.f/(t-b);
+    m[1][0] = m[1][2] = m[1][3] = 0.f;
+
+    m[2][2] = -2.f/(f-n);
+    m[2][0] = m[2][1] = m[2][3] = 0.f;
+    
+    m[3][0] = -(r+l)/(r-l);
+    m[3][1] = -(t+b)/(t-b);
+    m[3][2] = -(f+n)/(f-n);
+    m[3][3] = 1.f;
+}
 
 static void mat4x4_invert(float t[4][4], float m[4][4]) {
     float idet;
@@ -88,13 +105,38 @@ static void mat4x4_invert(float t[4][4], float m[4][4]) {
     t[3][2] = (-m[3][0] * s[3] + m[3][1] * s[1] - m[3][2] * s[0]) * idet;
     t[3][3] = ( m[2][0] * s[3] - m[2][1] * s[1] + m[2][2] * s[0]) * idet;
 }
-static void mat4x4_mul_vec4(float r[4], float m[4][4], const float v[4]) {
-    int i, j;
-    for(j=0; j<4; ++j) {
-        r[j] = 0.f;
-        for(i=0; i<4; ++i)
-            r[j] += m[i][j] * v[i];
-    }
+static void mat4x4_mul_vec(float m[4][4], float *r0, float *r1, float *r2) {
+    float v0 = *r0;
+    float v1 = *r1;
+    float v2 = *r2;
+    float v3 = 1.0f;
+    /* float r3 = 1.0f; */
+
+    *r0  = m[0][0] * v0;
+    *r0 += m[1][0] * v1;
+    *r0 += m[2][0] * v2;
+    *r0 += m[3][0] * v3;
+
+    *r1  = m[0][1] * v0;
+    *r1 += m[1][1] * v1;
+    *r1 += m[2][1] * v2;
+    *r1 += m[3][1] * v3;
+
+    *r2  = m[0][2] * v0;
+    *r2 += m[1][2] * v1;
+    *r2 += m[2][2] * v2;
+    *r2 += m[3][2] * v3;
+
+    /* r3 += m[0][3] * v0; */
+    /* r3 += m[1][3] * v1; */
+    /* r3 += m[2][3] * v2; */
+    /* r3 += m[3][3] * v3; */
+
+    /* if we did this we'd have to pass it to the GPU
+     * for e.g. perspective correct sampling anyway ... */
+    /* *r0 /= r3; */
+    /* *r1 /= r3; */
+    /* *r2 /= r3; */
 }
 
 
@@ -107,9 +149,62 @@ static void error_callback(int error, const char *description) {
 static struct {
     float mvp[4][4];
 
-    struct { float x; float y; } cursor; /* in world space, converted with mvp */
+    struct { float x, y, raw_x, raw_y; } cursor; /* in world space, converted with mvp */
     uint8_t keysdown[GLFW_KEY_LAST + 1];
 } input = {0};
+
+static void world_to_pixels(GLFWwindow *window, float mvp[4][4], float *x, float *y) {
+    {
+        float z = 0.0f;
+        mat4x4_mul_vec(mvp, x, y, &z);
+
+        /* -1..1 -> 0..1 */
+        *x = (*x * 0.5f) + 0.5f;
+        *y = (*y * 0.5f) + 0.5f;
+    }
+
+    {
+        int width, height;
+        glfwGetFramebufferSize(window, &width, &height);
+        *x *= (float)width;
+        *y *= (float)height;
+    }
+}
+
+static void screen_to_world(GLFWwindow *window, float *x, float *y) {
+    int width,  height;
+    glfwGetWindowSize(window, &width, &height);
+    *x = 0.0f + (*x / ((float)width ));
+    *y = 1.0f - (*y / ((float)height));
+
+    /* 0..1 -> -1..1 */
+    *x = *x*2.0f - 1.0f;
+    *y = *y*2.0f - 1.0f;
+
+    {
+        float z = 0.0f;
+        mat4x4_mul_vec(input.mvp, x, y, &z);
+    }
+}
+
+#if 0
+static void pixels_to_world(GLFWwindow *window, float *x, float *y) {
+    int width,  height;
+    glfwGetFramebufferSize(window, &width, &height);
+    *x = 0.0f + (*x / ((float)width ));
+    *y = 1.0f - (*y / ((float)height));
+
+    /* 0..1 -> -1..1 */
+    *x = *x*2.0f - 1.0f;
+    *y = *y*2.0f - 1.0f;
+
+    {
+        float z = 0.0f;
+        mat4x4_mul_vec(input.mvp, x, y, &z);
+    }
+}
+#endif
+
 static void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods) {
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GLFW_TRUE);
@@ -118,13 +213,8 @@ static void key_callback(GLFWwindow *window, int key, int scancode, int action, 
     input.keysdown[key] = !(action == GLFW_RELEASE);
 }
 static void cursor_position_callback(GLFWwindow *window, double xpos, double ypos) {
-    float v[4] = {0};
-    v[0] = xpos;
-    v[1] = ypos;
-    v[2] = 0;
-    v[3] = 1;
-
-    mat4x4_mul_vec4(v, input.mvp, v);
+    input.cursor.raw_x = xpos;
+    input.cursor.raw_y = ypos;
 }
 
 /* } PLATFORM */
@@ -180,6 +270,8 @@ static const char *fragment_shader_text =
 "}\n";
 
 typedef struct {
+    float mvp[4][4];
+
     uint32_t vbuf_count, ibuf_count;
     struct { GLuint vbuf, ibuf; } gpu;
     struct {
@@ -299,6 +391,91 @@ static void geo_circ(Geo *geo, float x, float y, float r, Color clr) {
     }
 }
 
+static void geo_line(
+    Geo *geo,
+    float x0, float y0,
+    float x1, float y1,
+    float thickness, Color clr
+) {
+    uint16_t start_i = geo->cpu.vbuf_i;
+
+    float dx = x0 - x1;
+    float dy = y0 - y1;
+
+    float px = -dy;
+    float py =  dx;
+
+    Vertex v = {0};
+
+    norm2(&px, &py);
+    px *= thickness / 2.0f;
+    py *= thickness / 2.0f;
+
+    v.rgba = clr;
+    v.pos.x = x0 + px, v.pos.y = y0 + py, geo->cpu.vbuf[geo->cpu.vbuf_i++] = v;
+    v.pos.x = x0 - px, v.pos.y = y0 - py, geo->cpu.vbuf[geo->cpu.vbuf_i++] = v;
+    v.pos.x = x1 + px, v.pos.y = y1 + py, geo->cpu.vbuf[geo->cpu.vbuf_i++] = v;
+    v.pos.x = x1 - px, v.pos.y = y1 - py, geo->cpu.vbuf[geo->cpu.vbuf_i++] = v;
+
+    geo->cpu.ibuf[geo->cpu.ibuf_i++] = 0 + start_i;
+    geo->cpu.ibuf[geo->cpu.ibuf_i++] = 1 + start_i;
+    geo->cpu.ibuf[geo->cpu.ibuf_i++] = 2 + start_i;
+    geo->cpu.ibuf[geo->cpu.ibuf_i++] = 2 + start_i;
+    geo->cpu.ibuf[geo->cpu.ibuf_i++] = 1 + start_i;
+    geo->cpu.ibuf[geo->cpu.ibuf_i++] = 3 + start_i;
+}
+
+/* pixel aligned! */
+static void geo_line_pxa(
+    Geo *geo,
+    float x0, float y0,
+    float x1, float y1,
+    float thickness, Color clr
+) {
+    uint16_t start_i = geo->cpu.vbuf_i;
+
+    float dx = x0 - x1;
+    float dy = y0 - y1;
+
+    float px = -dy;
+    float py =  dx;
+
+    Vertex v = {0};
+
+    norm2(&px, &py);
+    px *= thickness / 2.0f;
+    py *= thickness / 2.0f;
+
+    v.rgba = clr;
+
+    v.pos.x = x0 + px, v.pos.y = y0 + py;
+        v.pos.x = roundf(v.pos.x);
+        v.pos.y = roundf(v.pos.y);
+        geo->cpu.vbuf[geo->cpu.vbuf_i++] = v;
+
+    v.pos.x = x0 - px, v.pos.y = y0 - py;
+        v.pos.x = roundf(v.pos.x);
+        v.pos.y = roundf(v.pos.y);
+        geo->cpu.vbuf[geo->cpu.vbuf_i++] = v;
+
+    v.pos.x = x1 + px, v.pos.y = y1 + py;
+        v.pos.x = roundf(v.pos.x);
+        v.pos.y = roundf(v.pos.y);
+        geo->cpu.vbuf[geo->cpu.vbuf_i++] = v;
+
+    v.pos.x = x1 - px, v.pos.y = y1 - py;
+        v.pos.x = roundf(v.pos.x);
+        v.pos.y = roundf(v.pos.y);
+        geo->cpu.vbuf[geo->cpu.vbuf_i++] = v;
+
+    geo->cpu.ibuf[geo->cpu.ibuf_i++] = 0 + start_i;
+    geo->cpu.ibuf[geo->cpu.ibuf_i++] = 1 + start_i;
+    geo->cpu.ibuf[geo->cpu.ibuf_i++] = 2 + start_i;
+    geo->cpu.ibuf[geo->cpu.ibuf_i++] = 2 + start_i;
+    geo->cpu.ibuf[geo->cpu.ibuf_i++] = 1 + start_i;
+    geo->cpu.ibuf[geo->cpu.ibuf_i++] = 3 + start_i;
+}
+
 /* } GRAPHIX */
 
 
@@ -327,7 +504,7 @@ typedef struct {
              float y; float old_y; } cam;
 } Sim;
 
-void sim_init(Sim *sim) {
+static void sim_init(Sim *sim) {
     sim->cam.x = -(sim->actor.x = 0.0f);
     sim->cam.y = -(sim->actor.y = 0.0f);
 
@@ -335,7 +512,7 @@ void sim_init(Sim *sim) {
     sim->ticks = 1;
 }
 
-void sim_step(Sim *sim, double dt) {
+static void sim_step(Sim *sim, double dt) {
     /* avoid stepping more than 3 frames at once */
     dt = fmin(dt, FRAME_SECS * 3.0);
 
@@ -421,10 +598,13 @@ void sim_step(Sim *sim, double dt) {
 
 
 
+static void frame(Sim *sim, Geo *geo, GLFWwindow *window);
+static void frame_lines(Sim *sim, Geo *geo, Geo *world_geo, GLFWwindow *window);
 int main(void) {
     GLFWwindow *window;
     GLint program, mvp_location;
     Geo geo = {0};
+    Geo geo_screenspace = {0};
     Sim sim = {0};
 
     glfwSetErrorCallback(error_callback);
@@ -433,6 +613,7 @@ int main(void) {
         exit(EXIT_FAILURE);
 
     glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
+    glfwWindowHint(GLFW_SAMPLES, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 #ifndef WIN32
@@ -502,11 +683,11 @@ int main(void) {
 
     /* allocate dynamic, per-frame geometry buffer */
     geo_alloc(&geo, 1 << 9, 1 << 12);
+    geo_alloc(&geo_screenspace, 1 << 9, 1 << 12);
     sim_init(&sim);
 
     glfwSetTime(0);
     while (!glfwWindowShouldClose(window)) {
-        float mvp[4][4] = {0};
         float ratio;
         int width, height;
         {
@@ -526,79 +707,68 @@ int main(void) {
             1.0f
         );
 
-        /* generate dynamic frame geometry */
+        /* prepare mvps */
         {
-            geo_clear(&geo);
-#if 0
-            /* color swatches */
-            geo_box(&geo, sim.actor.x+1, sim.actor.y+1, 1.0f, 1.0f, COLOR_1);
-            geo_box(&geo, sim.actor.x+2, sim.actor.y+2, 1.0f, 1.0f, COLOR_2);
-            geo_box(&geo, sim.actor.x+3, sim.actor.y+3, 1.0f, 1.0f, COLOR_3);
-            geo_box(&geo, sim.actor.x+4, sim.actor.y+4, 1.0f, 1.0f, COLOR_4);
+            float zoom = 11.0f/2.0f; /* 10 units on screen */
+            mat4x4_ortho(
+                geo.mvp,
+                -ratio*zoom - sim.cam.x,
+                 ratio*zoom - sim.cam.x,
+                 -1.0f*zoom - sim.cam.y,
+                  1.0f*zoom - sim.cam.y,
+                  1.0f,
+                 -1.0f
+            );
 
-            geo_circ(&geo, sim.actor.x-1, sim.actor.y-1, 0.5f, COLOR_1);
-            geo_circ(&geo, sim.actor.x-2, sim.actor.y-2, 0.5f, COLOR_2);
-            geo_circ(&geo, sim.actor.x-3, sim.actor.y-3, 0.5f, COLOR_3);
-            geo_circ(&geo, sim.actor.x-4, sim.actor.y-4, 0.5f, COLOR_4);
-#else
-            geo_box(&geo, sim.actor.x, sim.actor.y, 1.0f, 1.0f, COLOR_2);
-            geo_box(&geo, input.cursor.x, input.cursor.y, 0.1f, 0.1f, COLOR_2);
-
-
-            {
-                int i;
-                for (i = 0; i < 25; i++) {
-                    float r = ((float)i / 25.0f) * 3.14159f * 2.0f;
-                    float x = cosf(r) * 5.0f;
-                    float y = sinf(r) * 5.0f;
-                    geo_circ(&geo, x, y, 0.6f, COLOR_5);
-                }
-            }
-#endif
-
-            geo_gl_upload(&geo);
-        }
-
-        /* prepare to draw dynamic geometry */
-        {
-            glUseProgram(program);
-            geo_gl_vertex_layout(&geo);
-        }
-
-        /* prepre mvp */
-        {
-            float zoom = 10.0f/2.0f; /* 10 units on screen */
-            float l = -ratio*zoom - sim.cam.x;
-            float r =  ratio*zoom - sim.cam.x;
-            float b =  -1.0f*zoom - sim.cam.y;
-            float t =   1.0f*zoom - sim.cam.y;
-            float n =   1.0f;
-            float f =  -1.0f;
-
-            mvp[0][0] = 2.f/(r-l);
-            mvp[0][1] = mvp[0][2] = mvp[0][3] = 0.f;
-
-            mvp[1][1] = 2.f/(t-b);
-            mvp[1][0] = mvp[1][2] = mvp[1][3] = 0.f;
-
-            mvp[2][2] = -2.f/(f-n);
-            mvp[2][0] = mvp[2][1] = mvp[2][3] = 0.f;
-
-            mvp[3][0] = -(r+l)/(r-l);
-            mvp[3][1] = -(t+b)/(t-b);
-            mvp[3][2] = -(f+n)/(f-n);
-            mvp[3][3] = 1.f;
+            mat4x4_ortho(
+                geo_screenspace.mvp,
+                0, width,
+                0, height,
+                1, -1
+            );
 
             {
                 float tmp[4][4] = {0};
-                memcpy(tmp, mvp, sizeof(mvp));
+                memcpy(tmp, geo.mvp, sizeof(geo.mvp));
                 mat4x4_invert(input.mvp, tmp);
             }
 
-            glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*) &mvp);
+            input.cursor.x = input.cursor.raw_x;
+            input.cursor.y = input.cursor.raw_y;
+            screen_to_world(window, &input.cursor.x, &input.cursor.y);
         }
 
-        glDrawElements(GL_TRIANGLES, geo.cpu.ibuf_i, GL_UNSIGNED_SHORT, 0);
+
+        {
+            /* generate screenspace geometry */
+            frame_lines(&sim, &geo_screenspace, &geo, window);
+
+            /* prepare to draw dynamic geometry */
+            {
+                glUseProgram(program);
+                geo_gl_vertex_layout(&geo_screenspace);
+            }
+
+            glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*) &geo_screenspace.mvp);
+            glEnable(0x809D);
+            glDrawElements(GL_TRIANGLES, geo_screenspace.cpu.ibuf_i, GL_UNSIGNED_SHORT, 0);
+
+        }
+
+        {
+            /* generate dynamic frame geometry */
+            frame(&sim, &geo, window);
+
+            /* prepare to draw dynamic geometry */
+            {
+                glUseProgram(program);
+                geo_gl_vertex_layout(&geo);
+            }
+
+            glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*) &geo.mvp);
+            glDrawElements(GL_TRIANGLES, geo.cpu.ibuf_i, GL_UNSIGNED_SHORT, 0);
+        }
+
 
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -608,4 +778,140 @@ int main(void) {
 
     glfwTerminate();
     exit(EXIT_SUCCESS);
+}
+
+#if 0
+void frame(Sim *sim, Geo *geo, GLFWwindow *window) {
+    geo_clear(geo);
+
+    geo_box(geo, input.cursor.x, input.cursor.y, 0.1f, 0.1f, COLOR_2);
+    geo_box(geo, sim->actor.x, sim->actor.y, 1.0f, 1.0f, COLOR_2);
+
+
+    {
+        int i;
+        for (i = 0; i < 25; i++) {
+            float r = ((float)i / 25.0f) * 3.14159f * 2.0f;
+            float x = cosf(r) * 5.0f;
+            float y = sinf(r) * 5.0f;
+            geo_circ(geo, x, y, 0.6f, COLOR_5);
+        }
+    }
+
+    geo_gl_upload(geo);
+
+    geo_box(&geo, input.cursor.x, input.cursor.y, 0.1f, 0.1f, COLOR_2);
+}
+#endif
+
+static void frame(Sim *sim, Geo *geo, GLFWwindow *window) {
+    geo_clear(geo);
+
+#if 0
+    /* color swatches */
+    geo_box(geo, sim.actor.x+1, sim.actor.y+1, 1.0f, 1.0f, COLOR_1);
+    geo_box(geo, sim.actor.x+2, sim.actor.y+2, 1.0f, 1.0f, COLOR_2);
+    geo_box(geo, sim.actor.x+3, sim.actor.y+3, 1.0f, 1.0f, COLOR_3);
+    geo_box(geo, sim.actor.x+4, sim.actor.y+4, 1.0f, 1.0f, COLOR_4);
+    geo_box(geo, sim.actor.x+5, sim.actor.y+5, 1.0f, 1.0f, COLOR_5);
+
+    geo_circ(geo, sim.actor.x-1, sim.actor.y-1, 0.5f, COLOR_1);
+    geo_circ(geo, sim.actor.x-2, sim.actor.y-2, 0.5f, COLOR_2);
+    geo_circ(geo, sim.actor.x-3, sim.actor.y-3, 0.5f, COLOR_3);
+    geo_circ(geo, sim.actor.x-4, sim.actor.y-4, 0.5f, COLOR_4);
+    geo_circ(geo, sim.actor.x-5, sim.actor.y-5, 0.5f, COLOR_5);
+#endif
+
+#if 0
+    /* draw border around screen */
+    {
+        float br[4] = { 1.0f, -1.0f, 0.0f, 1.0f};
+        float bl[4] = {-1.0f, -1.0f, 0.0f, 1.0f};
+        float tl[4] = {-1.0f,  1.0f, 0.0f, 1.0f};
+        float tr[4] = { 1.0f,  1.0f, 0.0f, 1.0f};
+
+        mat4x4_mul_vec(input.mvp, &br[0], &br[1], &br[2]);
+        mat4x4_mul_vec(input.mvp, &bl[0], &bl[1], &bl[2]);
+        mat4x4_mul_vec(input.mvp, &tl[0], &tl[1], &tl[2]);
+        mat4x4_mul_vec(input.mvp, &tr[0], &tr[1], &tr[2]);
+
+        geo_line(geo, br[0], br[1], bl[0], bl[1], 0.15f, COLOR_2);
+        geo_line(geo, bl[0], bl[1], tl[0], tl[1], 0.15f, COLOR_2);
+        geo_line(geo, tl[0], tl[1], tr[0], tr[1], 0.15f, COLOR_2);
+        geo_line(geo, tr[0], tr[1], br[0], br[1], 0.15f, COLOR_2);
+    }
+#endif
+
+    /* pentagram */
+    {
+        int i, j;
+        for (i = 0; i < 5; i++) {
+            float r = ((float)i / 5.0f) * 3.14159f * 2.0f;
+            float x = cosf(r) * 5.0f;
+            float y = sinf(r) * 5.0f;
+
+            for (j = 0; j < 5; j++) {
+                float r0 = ((float)j / 5.0f) * 3.14159f * 2.0f;
+                float x0 = cosf(r0) * 5.0f;
+                float y0 = sinf(r0) * 5.0f;
+
+                geo_line(geo, x, y, x0, y0, 0.05f, COLOR_1);
+            }
+        }
+    }
+
+    if (0) {
+        geo_circ(geo, 0.0f, 0.0f, 1.00f, COLOR_1);
+        geo_circ(geo, 0.0f, 0.0f, 0.95f, COLOR_0);
+
+    }
+
+    geo_box(geo, input.cursor.x, input.cursor.y, 0.2f, 0.2f, COLOR_2);
+
+    geo_gl_upload(geo);
+}
+
+static void frame_lines(Sim *sim, Geo *geo, Geo *world_geo, GLFWwindow *window) {
+    geo_clear(geo);
+
+    if (0) {
+        int width, height;
+        glfwGetFramebufferSize(window, &width, &height);
+
+        /* geo_box(geo, 0, 0, width, height, COLOR_3); */
+
+        geo_line_pxa(geo, 0.0f, 0.0f, width, height, 2.0f, COLOR_2);
+    }
+
+    {
+        float br[4] = { 1.0f, -1.0f, 0.0f, 1.0f};
+        float tl[4] = {-1.0f,  1.0f, 0.0f, 1.0f};
+
+        mat4x4_mul_vec(input.mvp, &br[0], &br[1], &br[2]);
+        mat4x4_mul_vec(input.mvp, &tl[0], &tl[1], &tl[2]);
+
+        {
+            int i;
+            float x_max = roundf(br[0])+1;
+            float x_min = roundf(tl[0])-1;
+            float y_max = roundf(tl[1])+1;
+            float y_min = roundf(br[1])-1;
+            float x_first = roundf(x_min) + 1;
+            float y_first = roundf(y_min) + 1;
+            float step_x, step_y;
+
+            world_to_pixels(window, world_geo->mvp, &x_max, &y_max);
+            world_to_pixels(window, world_geo->mvp, &x_min, &y_min);
+            world_to_pixels(window, world_geo->mvp, &x_first, &y_first);
+            step_x = x_first - x_min;
+            step_y = y_first - y_min;
+
+            for (i = x_min; i <= x_max; i += step_x)
+                geo_line_pxa(geo, i, y_min-1, i, y_max+1, 2.0f, COLOR_1);
+
+            for (i = y_min; i <= y_max; i += step_y)
+                geo_line_pxa(geo, x_min-1, i, x_max+1, i, 2.0f, COLOR_1);
+        }
+    }
+    geo_gl_upload(geo);
 }
